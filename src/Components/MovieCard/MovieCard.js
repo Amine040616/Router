2e513ce4e4fcb188204be196df69472ed5493ad1
Dell <dell@PCB-LEARN-DEV-08.groupe-atlas.com>
2020-12-23 14:10:03 +0100

    avec souci de style

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import img from './TheLittleThingsposter.jpg';
+import './MovieCard.css'
+
+
 function MovieCard (){
     return(
         <div>
-            <h3>MovieCard</h3>
+            <div className='posterStyle'>
+                <img src={img} alt="Movie" style={{width:'500px'}} /></div>
+            <h3>Titre du film</h3>
+            <span>Year of release</span><span>Movie Type</span>
+            <span>Rating un span pr le moment</span>
+            <p>Movie description</p>
         </div>
     )
     }
